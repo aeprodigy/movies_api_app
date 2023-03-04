d463c4ad601ad799e317042c6b71c3a7a89f98ea
mike mambwe <mikeapple056@gmail.com>
2023-03-04 10:32:47 +0200

    The Navbar of the app

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar/Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="header">
+      <header>
+        <nav>
+          <h3>|TheCream Joint|</h3>
+          <div className="menu">
+            <ul>
+              <li>About us</li>
+              <li>Contact</li>
+              <li>Careers</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
