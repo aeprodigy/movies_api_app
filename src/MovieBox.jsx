5dcb3cbd48d4df6ee628c81ca7d7a2fcd3a958cc
mike mambwe <mikeapple056@gmail.com>
2023-03-04 10:33:59 +0200

    the component with the api links

---
+++ src/MovieBox.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const api_img = "https://image.tmdb.org/t/p/w500/";
+
+const MovieBox = ({
+  title,
+  name,
+  poster_path,
+  vote_average,
+  release_date,
+  overview,
+}) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <img src={api_img + poster_path} alt=""></img>
+      <p>{overview}</p>
+      <h6>{vote_average}Votes</h6>
+    </div>
+  );
+};
+
+export default MovieBox;
