1e461ba12d68186784333dfe3f367f55db2bffb6
mike mambwe <mikeapple056@gmail.com>
2023-03-04 12:38:49 +0200

    Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Navbar/Navbar.css";
+import {FcFilmReel} from 'react-icons/fc';
 const Navbar = () => {
   return (
     <div className="header">
       <header>
         <nav>
-          <h3 className="logo">|TheCream Joint|</h3>
+          <h3 className="logo">
+            |<FcFilmReel />
+            MovieZ<span className="txt">PLace</span>|
+          </h3>
           <div className="menu">
             <ul>
               <li>About us</li>
